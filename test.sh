503303e4e28da8faab551abbf0956fcaf876535a
userdocs <16525024+userdocs@users.noreply.github.com>
2025-01-24 21:35:21 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
 
 if [[ -n "${GITHUB_REPOSITORY}" ]]; then echo "#################### yes"; fi
 
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ $GITHUB_REPOSITORY"
+
 echo "@@@@@@@@@@@@@@@@@@@@@ $(env &> /dev/null && echo $GITHUB_REPOSITORY)"
