a09a5483c41705c6dfefd4ed261fb8def85387ad
userdocs <16525024+userdocs@users.noreply.github.com>
2025-01-24 21:29:11 +0000

    test

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-env
+if [[ -n "${GITHUB_REPOSITORY}" ]]; then echo "#################### yes"; fi
+
+echo "@@@@@@@@@@@@@@@@@@@@@ $(env &> /dev/null && echo $HOGITHUB_REPOSITORYSTNAME)"
