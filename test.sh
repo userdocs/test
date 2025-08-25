df3abd41d1fdeb93391e7e10cf3a3a874bac260b
userdocs <16525024+userdocs@users.noreply.github.com>
2025-08-25 11:37:16 +0100

    test

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ -n "${GITHUB_REPOSITORY}" ]]; then echo "#################### yes"; fi
-
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ $GITHUB_REPOSITORY"
-
-echo "@@@@@@@@@@@@@@@@@@@@@ $(env &> /dev/null && echo $GITHUB_REPOSITORY)"
